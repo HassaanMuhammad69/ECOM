483acd608a3ba8b231c58aee6252ced371430630
HASSAAN MUHAMMAD <hassaans299@gmail.com>
2023-07-18 15:42:54 +0500

    key error resolve

--- web/src/components/user/userHome.jsx
+++ web/src/components/user/userHome.jsx
@@ -72,8 +72,8 @@ function Userhome() {
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
           {products.map((eachProduct, i) => {
             return (
-              <>
-                <div className='w-full shadow-xl bg-gray-800 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
+              
+                <div  key={i} className='w-full shadow-xl bg-gray-800 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
                   <img className='w-40 mx-auto mt-[-3rem] bg-transparent' alt="products" src={eachProduct.pictureUrl} />
                   <h2 className='text-2xl font-bold text-center py-8 text-white'>{eachProduct.name}</h2>
                   <p className='text-center text-4xl border-b font-bold text-white'>{eachProduct.price}</p>
@@ -87,7 +87,7 @@ function Userhome() {
                     <Link to={`/gallery`}>ADD TO CART</Link>
                     </button>
                 </div>
-              </>
+              
             )
           }
           )
